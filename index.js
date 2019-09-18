c8daeb1e815a92ff44c0f5bfe750759e7505093d
Taku Nakagawa <takunakagawa@TakunoMac-mini.local>
2019-09-18 22:12:40 +0900

    各Moduleのインストール

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 "use strict";
 
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
+const https = require("https");
+const request = require("request");
+const qs = require("querystring");
 const express = require("express");
 const server = express();
+
+const APIID = process.env.APIID;
+const SERVERID = process.env.SERVERID;
+const CONSUMERKEY = process.env.CONSUMERKEY;
+const PRIVATEKEY = process.env.PRIVATEKEY;
+const BOTNO = process.env.BOTNO;
 
 server.listen(process.env.PORT || 3000);
 
