f1da7fdb8e1f7c18a83a9f5ec4f0bdb5421933a8
Taku Nakagawa <takunakagawa@TakunoMac-mini.local>
2019-09-18 22:27:55 +0900

    Index.jsファイルの作成

--- index.js
+++ index.js
@@ -18,4 +18,58 @@ server.listen(process.env.PORT || 3000);
 
 server.post("/callback", (req, res) => {
   res.sendStatus(200);
+  const message = req.body.content.text;
+  const roomId = req.body.source.roomId;
+  const accountId = req.body.source.accountId;
+  getJWT(jwttoken => {
+    getServerToken(jwttoken, newtoken => {
+      fetchTelNumber(qnaInput, newtoken, accountId);
+    });
+  });
 });
+
+// JWTの更新と取得
+function getJWT(callback) {
+  const iss = SERVERID;
+  const iat = Math.floor(Date.now() / 1000);
+  const exp = iat + 60 * 60; //JWTの有効期間は1時間
+  const cert = Buffer.from(PRIVATEKEY.replace(/\\n/g, "\n"));
+  jwt.sign(
+    { iss: iss, iat: iat, exp: exp },
+    cert,
+    { algorithm: "RS256" },
+    (err, jwttoken) => {
+      if (!err) {
+        callback(jwttoken);
+      } else {
+        console.log("getJWT Error: ", err);
+      }
+    }
+  );
+}
+
+// Server Tokenの取得
+function getServerToken(jwttoken, callback) {
+  const postdata = {
+    url: "https://authapi.worksmobile.com/b/" + APIID + "/server/token",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
+    },
+    form: {
+      grant_type: encodeURIComponent(
+        "urn:ietf:params:oauth:grant-type:jwt-bearer"
+      ),
+      assertion: jwttoken
+    }
+  };
+  request.post(postdata, (error, response, body) => {
+    if (error) {
+      console.log("Error getServerToken: ", error);
+      callback(error);
+    } else {
+      const jsonobj = JSON.parse(body);
+      const AccessToken = jsonobj.access_token;
+      callback(AccessToken);
+    }
+  });
+}
