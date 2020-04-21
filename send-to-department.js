8c2d243cf272e9b512db629529a4b0421a0cbdd2
“OIDEMA” <k-goda@oidema-jp.com>
2020-04-21 10:03:02 +0900

    state back to normal

--- send-to-department.js
+++ send-to-department.js
@@ -22,9 +22,6 @@ module.exports = function sendToDepartment(messageText, token, accountId) {
     if (err) {
       console.log("error send message: ", err);
       return;
-    } else if (messageText === "利用開始") {
-      console.log('利用開始メッセージは送りません。');
-      process.exit(0);
     }
   });
 };
