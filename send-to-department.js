ff1b09fd40b00e5061d0a0a77ef4a6b75298a812
“OIDEMA” <k-goda@oidema-jp.com>
2020-04-21 09:56:21 +0900

    message exception added

--- send-to-department.js
+++ send-to-department.js
@@ -18,13 +18,14 @@ module.exports = function sendToDepartment(messageText, token, accountId) {
       }
     }
   };
-  request.post(postData, (err, response, body) => {
-    if (err) {
-      console.log("error send message: ", err);
-      return;
-    } else if (messageText === "利用開始") {
-      console.log('利用開始メッセージは送りません。')
-      process.exit(0);
-    }
-  });
+  if (messageText  === "利用開始") {
+    console.log("'利用開始メッセージは送りません。'");
+  } else {
+    request.post (postData, (err, response, body) => {
+      if (err) {
+        console.log("error send message: ", err);
+        return;
+      };
+    });
+  }
 };
