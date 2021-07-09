c4ac588132fd09f3c28edec13a3e4e8f62f70707
anabuki-kosan <anabuki-kosan@anabuki-kosannoMacBook-Pro.local>
2021-07-09 22:44:40 +0900

    test

--- share-sender.js
+++ share-sender.js
@@ -3,7 +3,7 @@ module.exports = function shareSender(token, accountId, contentType) {
     const BOTNO = process.env.BOTNO;
     const APIID = process.env.APIID;
     const CONSUMERKEY = process.env.CONSUMERKEY;
-    
+
     const postData = {
         url: "https://apis.worksmobile.com/" + APIID + "/message/sendMessage/v2",
         headers: {
@@ -15,7 +15,7 @@ module.exports = function shareSender(token, accountId, contentType) {
         roomId: process.env.LINE_IT_TALKROOMID,
         content: {
             type: "text",
-            text: accountId + "さんから" + contentType + "が送信されました。"
+            text: await getAccountInfo()
         }
         }
     };
@@ -23,4 +23,22 @@ module.exports = function shareSender(token, accountId, contentType) {
         console.log("error send message: ", err);
         return;
     });
+    async function getAccountInfo() {
+        try {
+          const account = await axios({
+            method: 'get',
+            url: `https://apis.worksmobile.com/r/${APIID}/contact/v2/accounts/${accountId}`,
+            headers: {
+              "Content-Type": "application/json;charset=UTF-8",
+              consumerKey: CONSUMERKEY,
+              Authorization: "Bearer " + token
+            }
+          }).then((res) => {
+            return res.data
+          })
+          return "ID：" + accountId  + "\n" + `${account.name}さんが${contentType}を送信しました。` +  account.name + "\n" +　"所属部署：" + account.representOrgUnitName
+        } catch (e) {
+          console.log(e)
+        }
+      }
 };
